950f06dcf3178016e048e3218e3e069a8eff6187
Aadameqq <adamek5599.sluzbowy@gmail.com>
2022-07-20 00:09:53 +0200

    setup new frontend dev environment
     Author:    Aadameqq <adamek5599.sluzbowy@gmail.com>
    
     interactive rebase in progress; onto a67f9fb
     Last command done (1 command done):
        pick 7bb8560 setup new frontend dev environment
     No commands remaining.
     You are currently rebasing branch '003/create-dev-environment' on 'a67f9fb'.
    
     Changes to be committed:
    	new file:   .eslintrc.js
    	new file:   .jest/setup.ts
    	new file:   .prettierrc
    	new file:   README.md
    	new file:   babel.config.jest.js
    	new file:   jest.config.js
    	new file:   next-env.d.ts
    	new file:   next.config.js
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   pages/_app.tsx
    	new file:   pages/api/hello.ts
    	new file:   pages/index.tsx
    	new file:   postcss.config.js
    	new file:   public/favicon.ico
    	new file:   public/vercel.svg
    	new file:   styles/global.scss
    	new file:   styles/home.module.scss
    	new file:   tailwind.config.js
    	new file:   tsconfig.json

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './pages/**/*.{html,js,ts,tsx,jsx}',
+    './components/**/*.{html,js,ts,tsx,jsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
