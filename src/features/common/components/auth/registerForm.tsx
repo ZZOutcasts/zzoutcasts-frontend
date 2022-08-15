616d9be4cb74e695ab0da787b9b79322dcde4a77
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-08-15 20:44:02 +0200

    Add complete registerForm

--- src/features/common/components/auth/registerForm.tsx
+++ src/features/common/components/auth/registerForm.tsx
@@ -1,5 +1,59 @@
-export const RegisterForm = ({}) => {
-  //code
+import { useToggle, upperFirst } from '@mantine/hooks'
+import { useForm } from '@mantine/form'
+import { routes } from '@config/routes'
+import {
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button
+} from '@mantine/core'
+import { ReactNode } from 'react'
 
-  return <p>Register Form</p>
+export function RegisterForm() {
+  return (
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900
+        })}
+      >
+        Welcome to Projectly!
+      </Title>
+      <Text color="dimmed" size="lg" align="center" mt={5}>
+        Alredy registerd ?{' '}
+        <Anchor<'a'>
+          //onClick={(event) => event.preventDefault()}
+          href={routes.login_demo()}
+          size="lg"
+        >
+          Log in
+        </Anchor>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Name" placeholder="Name" required />
+        <TextInput label="Email" placeholder="you@mail.com" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <Group position="apart" mt="md">
+          <Checkbox label="Remember me" />
+        </Group>
+        <Button fullWidth mt="xl">
+          Register
+        </Button>
+      </Paper>
+    </Container>
+  )
 }
