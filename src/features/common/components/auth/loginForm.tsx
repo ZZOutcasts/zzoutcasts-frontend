e2c14746cfde85a533516aa4e0c7b50067d9e93a
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-08-15 20:43:05 +0200

    change previous loginForm to simplier and cleaner solution

--- src/features/common/components/auth/loginForm.tsx
+++ src/features/common/components/auth/loginForm.tsx
@@ -1,121 +1,65 @@
-// export const LoginForm = ({}) => {
-//   //code
-
-//   return <p>Login Form</p>
-// }
-
 import { useToggle, upperFirst } from '@mantine/hooks'
 import { useForm } from '@mantine/form'
+import { routes } from '@config/routes'
 import {
   TextInput,
   PasswordInput,
-  Text,
-  Paper,
-  Group,
-  PaperProps,
-  Button,
-  Divider,
   Checkbox,
   Anchor,
-  Stack
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button
 } from '@mantine/core'
 import { ReactNode } from 'react'
-//import { GoogleButton, TwitterButton } from '../SocialButtons/SocialButtons';
 
-export function LoginForm(props: PaperProps<any>) {
-  const [type, toggle]: any = useToggle(['login', 'register'])
-  const form = useForm({
-    initialValues: {
-      email: '',
-      name: '',
-      password: '',
-      terms: true
-    },
-
-    validate: {
-      email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
-      password: (val) =>
-        val.length <= 6 ? 'Password should include at least 6 characters' : null
-    }
-  })
-
+export function LoginForm() {
   return (
-    <Paper radius="md" p="xl" withBorder {...props}>
-      <Text size="lg" weight={500}>
-        Welcome to Projectly, Login with
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900
+        })}
+      >
+        Welcome back!
+      </Title>
+      <Text color="dimmed" size="lg" align="center" mt={5}>
+        Do not have an account yet?{' '}
+        <Anchor<'a'>
+          //onClick={(event) => event.preventDefault()}
+          href={routes.register_demo()}
+          size="lg"
+        >
+          Create account
+        </Anchor>
       </Text>
 
-      <Group grow mb="md" mt="md">
-        {/* <GoogleButton radius="xl">Google</GoogleButton>
-        <TwitterButton radius="xl">Twitter</TwitterButton>*/}
-      </Group>
-
-      <Divider label="Or continue with email" labelPosition="center" my="lg" />
-
-      <form onSubmit={form.onSubmit(() => {})}>
-        <Stack>
-          {
-            <TextInput
-              label="Name"
-              placeholder="Your name"
-              value={form.values.name}
-              onChange={(event) =>
-                form.setFieldValue('name', event.currentTarget.value)
-              }
-            />
-          }
-
-          <TextInput
-            required
-            label="Email"
-            placeholder="hello@mantine.dev"
-            value={form.values.email}
-            onChange={(event) =>
-              form.setFieldValue('email', event.currentTarget.value)
-            }
-            error={form.errors.email && 'Invalid email'}
-          />
-
-          <PasswordInput
-            required
-            label="Password"
-            placeholder="Your password"
-            value={form.values.password}
-            onChange={(event) =>
-              form.setFieldValue('password', event.currentTarget.value)
-            }
-            error={
-              form.errors.password &&
-              'Password should include at least 6 characters'
-            }
-          />
-
-          {
-            <Checkbox
-              label="I accept terms and conditions"
-              checked={form.values.terms}
-              onChange={(event) =>
-                form.setFieldValue('terms', event.currentTarget.checked)
-              }
-            />
-          }
-        </Stack>
-
-        <Group position="apart" mt="xl">
-          <Anchor
-            component="button"
-            type="button"
-            color="dimmed"
-            onClick={() => toggle()}
-            size="xs"
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="you@mail.com" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <Group position="apart" mt="md">
+          <Checkbox label="Remember me" />
+          <Anchor<'a'>
+            //onClick={(event) => event.preventDefault()}
+            href="#" // TODO: add route to "forgot password page"
+            size="sm"
           >
-            {type === 'register'
-              ? 'Already have an account? Login'
-              : "Don't have an account? Register"}
+            Forgot password?
           </Anchor>
-          {<Button type="submit">{upperFirst(type)}</Button>}
         </Group>
-      </form>
-    </Paper>
+        <Button fullWidth mt="xl">
+          Sign in
+        </Button>
+      </Paper>
+    </Container>
   )
 }
