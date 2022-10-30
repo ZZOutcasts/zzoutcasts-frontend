2726ff58e85381416be262e3eea3f82976967fd5
Aadameqq <adamek5599.sluzbowy@gmail.com>
2022-10-30 10:54:15 +0100

    replace status codes with enum

--- src/features/auth/hooks/useHandleForbidden.ts
+++ src/features/auth/hooks/useHandleForbidden.ts
@@ -1,13 +1,17 @@
 import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { noAuthRoute } from '@features/auth/utils/noAuthRoute'
+import { StatusCode } from '@utils/StatusCode'
 
 export const useHandleForbidden = (error?: AxiosError) => {
   const router = useRouter()
 
   if (!error) return
 
-  const statusCodesMeaningForbidden = [401, 403]
+  const statusCodesMeaningForbidden = [
+    StatusCode.FORBIDDEN,
+    StatusCode.UNAUTHORIZED
+  ]
 
   const statusCode = Number(error.status)
 
--- src/utils/StatusCode.ts
+++ src/utils/StatusCode.ts
@@ -1,4 +1,6 @@
 // eslint-disable-next-line no-shadow
 export enum StatusCode {
-  NOT_FOUND = 404
+  NOT_FOUND = 404,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403
 }
